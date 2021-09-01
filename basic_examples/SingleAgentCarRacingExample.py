19d5d572b9fc20f4d45b53c79362df0d473eb6ae
avcu <avcu@usc.edu>
2021-08-31 21:18:25 -0700

    adding state and action variables to example run

--- basic_examples/SingleAgentCarRacingExample.py
+++ basic_examples/SingleAgentCarRacingExample.py
@@ -1,7 +1,17 @@
 import gym
 env = gym.make('CarRacing-v0')
-env.reset()
-for _ in range(1000):
+init_observation = env.reset()
+total_reward = 0
+for cur_time in range(1000):
     env.render()
-    env.step(env.action_space.sample())  # take a random action
+
+    cur_action = env.action_space.sample() # take a random action
+    observation, reward, is_done, info = env.step(cur_action)
+    total_reward = total_reward + reward
+    print('Current reward: {}'.format(reward))
+
+    if is_done:
+        print('Episode is finised after {}th timesteps'.format(cur_time))
+        print('Total reward: {}'.format(total_reward))
+
 env.close()
