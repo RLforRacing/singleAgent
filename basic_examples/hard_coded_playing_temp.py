5d0e152a25ddc462476425f13f89ca52923b8698
avcu <avcu@usc.edu>
2021-09-06 01:56:44 -0700

    updated parameters for the hard-coded player

--- basic_examples/hard_coded_playing_temp.py
+++ basic_examples/hard_coded_playing_temp.py
@@ -20,10 +20,15 @@ print('Lowest value for observation: {}'.format(np.min(env.observation_space.low
 print('Highest value for observation: {}'.format(np.min(env.observation_space.high)))
 
 # some discrete actions below
-left_soft_speed_soft = [-0.8, 0.3, 0.0]
-right_soft_speed_soft = [0.8, 0.3, 0.0]
-accelarate = [0.0, 1.0, 0.2]
-brake = [0.0, 0.0, 0.8]
+left_curve_speed_soft = [-0.75, 0.0, 0.0]
+right_curve_speed_soft = [0.75, 0.0, 0.0]
+left_soft_v1 = [-0.5, 0.0, 0.0]
+right_soft_v1 = [0.5, 0.0, 0.0]
+left_soft_v2 = [-0.25, 0.0, 0.0]
+right_soft_v2 = [0.25, 0.0, 0.0]
+accelarate = [0.0, 0.8, 0.15]
+accelarate_v2 = [0.0, 0.6, 0.15]
+brake = [0.0, 0.0, 0.15]
 
 for cur_time in range(T_max):
     print('Time: {}'.format(cur_time))
@@ -31,8 +36,10 @@ for cur_time in range(T_max):
 
     if cur_time < 50:
         print('Taking random action')
-        cur_action = env.action_space.sample() # take a random action
-    observation, reward, is_done, info = env.step(cur_action)
+        next_action = env.action_space.sample() # take a random action
+    else:
+        next_action = cur_action
+    observation, reward, is_done, info = env.step(next_action)
     observation = strip_indicators(observation)  # Get rid of indicator bar on the bottom
     filtered = detect_edge(observation)  # Detect edges for vis
     car_loc = locate_car(observation)
@@ -51,21 +58,37 @@ for cur_time in range(T_max):
     #     print('Braking')
     #     cur_action = brake
 
+    # if dist > 20 and dist < 25:
+    #     print('CURVE AHEAD, BRAKE')
+    #     cur_action = brake
     if dist > 1 and dist < 20 and right_dist > left_dist + 1:
         print('CURVE AHEAD, Turning right')
-        cur_action = right_soft_speed_soft
+        cur_action = right_curve_speed_soft
     elif dist > 1 and dist < 20 and left_dist > right_dist + 1:
         print('CURVE AHEAD, Turning left')
-        cur_action = left_soft_speed_soft
-    elif right_dist > 0 and right_dist < 2:
+        cur_action = left_curve_speed_soft
+
+    elif right_dist >= 2 and right_dist < 4:
         print('Getting close to Right, Turning left')
-        cur_action = left_soft_speed_soft
-    elif left_dist > 0 and left_dist < 2:
+        cur_action = left_soft_v2
+    elif left_dist >= 2 and left_dist < 4:
         print('Getting close to Left, Turning Right')
-        cur_action = right_soft_speed_soft
+        cur_action = right_soft_v2
+
+    elif right_dist >= 0 and right_dist < 2:
+        print('Getting close to Right, Turning left')
+        cur_action = left_soft_v1
+    elif left_dist >= 0 and left_dist < 2:
+        print('Getting close to Left, Turning Right')
+        cur_action = right_soft_v1
+
     else:
-        print('Accelerate')
-        cur_action = accelarate
+        if cur_time < 250:
+            print('Accelerate')
+            cur_action = accelarate
+        else:
+            print('Accelerate')
+            cur_action = accelarate_v2
     
     
     total_reward = total_reward + reward
