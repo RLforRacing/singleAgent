b5c572119c9822012e2846fbf3fe4d22db09cf00
avcu <avcu@usc.edu>
2021-09-06 02:16:09 -0700

    making hard-coded agent smooth direction changes

--- basic_examples/hard_coded_playing_temp.py
+++ basic_examples/hard_coded_playing_temp.py
@@ -20,14 +20,14 @@ print('Lowest value for observation: {}'.format(np.min(env.observation_space.low
 print('Highest value for observation: {}'.format(np.min(env.observation_space.high)))
 
 # some discrete actions below
-left_curve_speed_soft = [-0.75, 0.0, 0.0]
-right_curve_speed_soft = [0.75, 0.0, 0.0]
-left_soft_v1 = [-0.5, 0.0, 0.0]
-right_soft_v1 = [0.5, 0.0, 0.0]
-left_soft_v2 = [-0.25, 0.0, 0.0]
-right_soft_v2 = [0.25, 0.0, 0.0]
+left_curve_speed_soft = [-0.85, 0.0, 0.0]
+right_curve_speed_soft = [0.85, 0.0, 0.0]
+left_soft_v1 = [-0.3, 0.0, 0.03]
+right_soft_v1 = [0.3, 0.0, 0.03]
+left_soft_v2 = [-0.15, 0.0, 0.02]
+right_soft_v2 = [0.15, 0.0, 0.02]
 accelarate = [0.0, 0.8, 0.15]
-accelarate_v2 = [0.0, 0.6, 0.15]
+accelarate_v2 = [0.0, 0.75, 0.15]
 brake = [0.0, 0.0, 0.15]
 
 for cur_time in range(T_max):
@@ -53,35 +53,29 @@ for cur_time in range(T_max):
     #     plt.imshow(filtered, cmap='gray')
     #     plt.show()
     #     plt.pause(0.1)
-    
-    # if dist > 1 and dist < 30:
-    #     print('Braking')
-    #     cur_action = brake
 
-    # if dist > 20 and dist < 25:
-    #     print('CURVE AHEAD, BRAKE')
-    #     cur_action = brake
+    ## There is a curve ahead, either turn right or left
     if dist > 1 and dist < 20 and right_dist > left_dist + 1:
         print('CURVE AHEAD, Turning right')
         cur_action = right_curve_speed_soft
     elif dist > 1 and dist < 20 and left_dist > right_dist + 1:
         print('CURVE AHEAD, Turning left')
         cur_action = left_curve_speed_soft
-
+    ## Slightly off to the center of the road, small direction change
     elif right_dist >= 2 and right_dist < 4:
         print('Getting close to Right, Turning left')
         cur_action = left_soft_v2
     elif left_dist >= 2 and left_dist < 4:
         print('Getting close to Left, Turning Right')
         cur_action = right_soft_v2
-
+    ## Quite off to the center of the road, high direction change
     elif right_dist >= 0 and right_dist < 2:
         print('Getting close to Right, Turning left')
         cur_action = left_soft_v1
     elif left_dist >= 0 and left_dist < 2:
         print('Getting close to Left, Turning Right')
         cur_action = right_soft_v1
-
+    ## All fine, get faster
     else:
         if cur_time < 250:
             print('Accelerate')
@@ -89,7 +83,6 @@ for cur_time in range(T_max):
         else:
             print('Accelerate')
             cur_action = accelarate_v2
-    
     
     total_reward = total_reward + reward
 
